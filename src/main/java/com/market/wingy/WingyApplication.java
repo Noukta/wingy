cdcf4fe894ca446ecae64b5746f7787ecd7133e2
Ahmed Noukta <no9ta-ahmed@live.fr>
2024-12-24 21:27:44 +0100

    Initial commit

---
+++ src/main/java/com/market/wingy/WingyApplication.java
@@ -0,0 +1,13 @@
+package com.market.wingy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WingyApplication.class, args);
+	}
+
+}
